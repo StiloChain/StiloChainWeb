4fc0aa376f0ac5203b80d1b50a22f8457ae15d3f
Omar-RS19 <146732223+Omar-RS19@users.noreply.github.com>
2023-11-14 19:33:16 -0500

    Update LlamarHorarios.js

--- Servicios/JS/LlamarHorarios.js
+++ Servicios/JS/LlamarHorarios.js
@@ -3,7 +3,7 @@ var variable = [];
 
 btn.addEventListener("click", function () {
     var Servicio = document.getElementById("Servicio").value;
-    fetch("http://localhost:8084/empleado/" + Servicio)
+    fetch("http://localhost:8084/stilochain-0.0.1-SNAPSHOT/empleado/" + Servicio)
         .then(res => res.json())
         .then(res => {
             variable.push(res.ocuemp);
